58760cf93eb02abebd07b30e7ba5037b6e5c45a6
Lakshmi Krishnamurthy <lakshmi.krishnamurthy@incapturetechnologies.com>
2016-12-03 12:04:12 -0500

    Features:
    	- Measure Gaussian Package Revision Stamp (1)
    	- Measure Lebesgue Package Revision Stamp (2)
    	- Measure Statistics Package Revision Stamp (3)
    	- Param Config Package Revision Stamp (4)
    	- Param Creator Package Revision Stamp (5)
    	- Param Definition Package Revision Stamp (6)
    	- Param Market Package Revision Stamp (7)
    	- Param Period Package Revision Stamp (8)
    	- Param Pricer Package Revision Stamp (9)
    	- Param Quote Package Revision Stamp (10)
    	- Param Quoting Package Revision Stamp (11)
    	- Param Valuation Package Revision Stamp (12)
    	- Portfolio Construction Allocator Revision Stamp (13)
    	- Portfolio Construction ALM Revision Stamp (14)
    	- Portfolio Construction Asset Revision Stamp (15)
    	- Portfolio Construction Bayesian Revision Stamp (16)
    	- Portfolio Construction MPT Revision Stamp (17)
    	- Portfolio Construction Params Revision Stamp (18)
    	- Pricer Option Package Revision Stamp (19)
    	- Product Calib Package Revision Stamp (20)
    	- Product Creator Package Revision Stamp (21)
    	- Product Credit Package Revision Stamp (22)
    	- Product Definition Package Revision Stamp (23)
    	- Product FRA Package Revision Stamp (24)
    	- Product FX Package Revision Stamp (25)
    	- Product Govvie Package Revision Stamp (26)
    	- Product Option Package Revision Stamp (27)
    	- Product Params Package Revision Stamp (28)
    	- Product Rates Package Revision Stamp (29)
    	- Quant Calculus Package Revision Stamp (30)
    	- Quant Common Package Revision Stamp (31)
    	- Quant Eigen Package Revision Stamp (32)
    	- Quant Fourier Package Revision Stamp (33)
    	- Quant Linear Algebra Revision Stamp (34)
    	- Quant Stochastic Package Revision Stamp (35)
    	- Regression Core Package Revision Stamp (36)
    	- Regression Curve Package Revision Stamp (37)
    	- Regression Curve Jacobian Revision Stamp (38)
    	- Regression Fixed Point Finder Stamp (39)
    	- Regression Spline Package Revision Stamp (40)
    	- Sample Algo Package Revision Stamp (41)
    	- Sample ALM Package Revision Stamp (42)
    	- Sample Almgren 2003 Revision Stamp (43)
    	- Sample Almgren 2012 Revision Stamp (44)
    	- Sample Almgren Chriss Revision Stamp (45)
    	- Sample Asset Allocation Revision Stamp (46)
    	- Sample Asset Allocation Excel Stamp (47)
    	- Sample Asset Backed Package Stamp (48)
    	- Sample ATHL Package Revision Stamp (49)
    	- Sample Black Litterman Revision Stamp (50)
    	- Sample BBG Package Revision Stamp (51)
    	- Sample Bond Package Revision Stamp (52)
    	- Sample Bond API Revision Stamp (53)
    	- Sample Cap Floor Revision Stamp (54)
    	- Sample Classifier Package Revision Stamp (55)
    	- Sample CMS Package Revision Stamp (56)
    	- Sample Collateral Package Revision Stamp (57)
    	- Sample Covering Number Revision Stamp (58)
    	- Sample Credit Package Revision Stamp (59)
    	- Sample Credit Feed Revision Stamp (60)
    
    
    Bug Fixes/Clean-up:
    
    Samples:

--- org/drip/product/creator/CDXRefDataHolder.java
+++ org/drip/product/creator/CDXRefDataHolder.java
@@ -10,16 +10,36 @@ package org.drip.product.creator;
  */
 
 /*!
+ * Copyright (C) 2017 Lakshmi Krishnamurthy
+ * Copyright (C) 2016 Lakshmi Krishnamurthy
  * Copyright (C) 2015 Lakshmi Krishnamurthy
  * Copyright (C) 2014 Lakshmi Krishnamurthy
  * Copyright (C) 2013 Lakshmi Krishnamurthy
  * Copyright (C) 2012 Lakshmi Krishnamurthy
- *
- * This file is part of CreditAnalytics, a free-software/open-source library for
- *		fixed income analysts and developers - http://www.credit-trader.org
- *
- * CreditAnalytics is a free, full featured, fixed income credit analytics library,
- * 		developed with a special focus towards the needs of the bonds and credit products community.
+ * 
+ *  This file is part of DRIP, a free-software/open-source library for buy/side financial/trading model
+ *  	libraries targeting analysts and developers
+ *  	https://lakshmidrip.github.io/DRIP/
+ *  
+ *  DRIP is composed of four main libraries:
+ *  
+ *  - DRIP Fixed Income - https://lakshmidrip.github.io/DRIP-Fixed-Income/
+ *  - DRIP Asset Allocation - https://lakshmidrip.github.io/DRIP-Asset-Allocation/
+ *  - DRIP Numerical Optimizer - https://lakshmidrip.github.io/DRIP-Numerical-Optimizer/
+ *  - DRIP Statistical Learning - https://lakshmidrip.github.io/DRIP-Statistical-Learning/
+ * 
+ *  - DRIP Fixed Income: Library for Instrument/Trading Conventions, Treasury Futures/Options,
+ *  	Funding/Forward/Overnight Curves, Multi-Curve Construction/Valuation, Collateral Valuation and XVA
+ *  	Metric Generation, Calibration and Hedge Attributions, Statistical Curve Construction, Bond RV
+ *  	Metrics, Stochastic Evolution and Option Pricing, Interest Rate Dynamics and Option Pricing, LMM
+ *  	Extensions/Calibrations/Greeks, Algorithmic Differentiation, and Asset Backed Models and Analytics.
+ * 
+ *  - DRIP Asset Allocation: Library for model libraries for MPT framework, Black Litterman Strategy
+ *  	Incorporator, Holdings Constraint, and Transaction Costs.
+ * 
+ *  - DRIP Numerical Optimizer: Library for Numerical Optimization and Spline Functionality.
+ * 
+ *  - DRIP Statistical Learning: Library for Statistical Evaluation and Machine Learning.
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *   	you may not use this file except in compliance with the License.
--- org/drip/sample/fixfloat/InAdvanceSwap.java
+++ org/drip/sample/fixfloat/InAdvanceSwap.java
@@ -30,11 +30,29 @@ import org.drip.state.inference.*;
  * Copyright (C) 2015 Lakshmi Krishnamurthy
  * Copyright (C) 2014 Lakshmi Krishnamurthy
  * 
- *  This file is part of DRIP, a free-software/open-source library for fixed income analysts and developers -
- * 		http://www.credit-trader.org/Begin.html
+ *  This file is part of DRIP, a free-software/open-source library for buy/side financial/trading model
+ *  	libraries targeting analysts and developers
+ *  	https://lakshmidrip.github.io/DRIP/
+ *  
+ *  DRIP is composed of four main libraries:
+ *  
+ *  - DRIP Fixed Income - https://lakshmidrip.github.io/DRIP-Fixed-Income/
+ *  - DRIP Asset Allocation - https://lakshmidrip.github.io/DRIP-Asset-Allocation/
+ *  - DRIP Numerical Optimizer - https://lakshmidrip.github.io/DRIP-Numerical-Optimizer/
+ *  - DRIP Statistical Learning - https://lakshmidrip.github.io/DRIP-Statistical-Learning/
  * 
- *  DRIP is a free, full featured, fixed income rates, credit, and FX analytics library with a focus towards
- *  	pricing/valuation, risk, and market making.
+ *  - DRIP Fixed Income: Library for Instrument/Trading Conventions, Treasury Futures/Options,
+ *  	Funding/Forward/Overnight Curves, Multi-Curve Construction/Valuation, Collateral Valuation and XVA
+ *  	Metric Generation, Calibration and Hedge Attributions, Statistical Curve Construction, Bond RV
+ *  	Metrics, Stochastic Evolution and Option Pricing, Interest Rate Dynamics and Option Pricing, LMM
+ *  	Extensions/Calibrations/Greeks, Algorithmic Differentiation, and Asset Backed Models and Analytics.
+ * 
+ *  - DRIP Asset Allocation: Library for model libraries for MPT framework, Black Litterman Strategy
+ *  	Incorporator, Holdings Constraint, and Transaction Costs.
+ * 
+ *  - DRIP Numerical Optimizer: Library for Numerical Optimization and Spline Functionality.
+ * 
+ *  - DRIP Statistical Learning: Library for Statistical Evaluation and Machine Learning.
  * 
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *   	you may not use this file except in compliance with the License.
