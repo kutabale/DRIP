2ab583fffe04e880d2d217eedd2a4dd659f0ceff
lakshmiDRIP <Lakshmi Krishnamurthy>
2017-03-09 12:05:42 -0500

    Features:
    Bug Fixes/Clean-up:
    
    Samples:
    
    	- Uncollateralized Par Increment #1 (1, 2)
    	- Uncollateralized Par Increment #2 (3, 4)
    	- Uncollateralized Par Increment #3 (5, 6)
    	- Uncollateralized Par Increment #4 (7, 8)
    	- Uncollateralized Par Increment #5 (9, 10)
    	- Uncollateralized Par Increment #6 (11, 12)
    	- Uncollateralized Par Increment #7 (13, 14)
    	- Uncollateralized Par Increment #8 (15, 16)
    	- Uncollateralized Par Increment #9 (17, 18)
    	- Uncollateralized Par Increment #10 (19, 20)
    	- Uncollateralized Par Increment #11 (21, 22)
    	- Uncollateralized Par Increment #12 (23, 24)
    	- Uncollateralized Par Increment #13 (25, 26)
    	- Uncollateralized Par Increment #14 (27, 28)
    	- Uncollateralized Par Increment #15 (29, 30)
    	- Uncollateralized Par Increment #16 (31, 32)
    	- Uncollateralized Par Increment #17 (33, 34)
    	- Uncollateralized Par Increment #18 (35, 36)
    	- Uncollateralized Par Increment #19 (37, 38)
    	- Uncollateralized Par Increment #20 (39, 40)
    	- Uncollateralized Par Increment #21 (41, 42)
    	- Uncollateralized Par Increment #22 (43, 44)
    	- Uncollateralized Par Increment #23 (45, 46)
    	- Uncollateralized Par Increment #24 (47, 48)
    	- Uncollateralized Par Increment #25 (49, 50)
    	- Uncollateralized Par Increment #26 (51, 52)
    	- Uncollateralized Par Increment #27 (53, 54)
    	- Uncollateralized Par Increment #28 (55, 56)
    	- Uncollateralized Par Increment #29 (57, 58)
    	- Uncollateralized Par Increment #30 (59, 60)

--- org/drip/sample/basel/UncollateralizedParIncrement.java
+++ org/drip/sample/basel/UncollateralizedParIncrement.java
@@ -1,8 +1,17 @@
 
 package org.drip.sample.basel;
 
+import org.drip.analytics.date.*;
+import org.drip.measure.crng.RandomNumberGenerator;
+import org.drip.measure.discrete.CorrelatedPathVertexDimension;
+import org.drip.measure.dynamics.*;
 import org.drip.measure.process.DiffusionEvolver;
 import org.drip.measure.realization.*;
+import org.drip.measure.statistics.UnivariateDiscreteThin;
+import org.drip.quant.common.FormatUtil;
+import org.drip.quant.linearalgebra.Matrix;
+import org.drip.service.env.EnvManager;
+import org.drip.xva.trajectory.*;
 
 /*
  * -*- mode: java; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4 -*-
@@ -162,5 +171,517 @@ public class UncollateralizedParIncrement {
 				dblSwapNotional * dblTimeWidth * (iMaturityStep - j) * adblATMSwapRateOffsetRealization[j];
 
 		return adblSwapPortfolioValueRealization;
+	}
+
+	private static final double[][] Path (
+		final double[][] aadblCorrelation,
+		final int iNumVertex)
+		throws Exception
+	{
+		CorrelatedPathVertexDimension cpvd = new CorrelatedPathVertexDimension (
+			new RandomNumberGenerator(),
+			aadblCorrelation,
+			iNumVertex,
+			1,
+			false,
+			null
+		);
+
+		return cpvd.multiPathVertexRd()[0].flatform();
+	}
+
+	private static final CounterPartyGroupAggregator Mix (
+		final double dblTimeMaturity1,
+		final double dblATMSwapRateOffsetStart1,
+		final double dblSwapNotional1,
+		final double dblTimeMaturity2,
+		final double dblATMSwapRateOffsetStart2,
+		final double dblSwapNotional2)
+		throws Exception
+	{
+		int iNumStep = 10;
+		int iNumPath = 100000;
+		int iNumVertex = 10;
+		double dblTime = 5.;
+		double dblATMSwapRateOffsetDrift = 0.0;
+		double dblATMSwapRateOffsetVolatility = 0.25;
+		double dblCSADrift = 0.01;
+		double dblCSAVolatility = 0.05;
+		double dblCSAInitial = 1.;
+		double dblBankHazardRateDrift = 0.002;
+		double dblBankHazardRateVolatility = 0.20;
+		double dblBankHazardRateInitial = 0.015;
+		double dblBankRecoveryRateDrift = 0.002;
+		double dblBankRecoveryRateVolatility = 0.02;
+		double dblBankRecoveryRateInitial = 0.40;
+		double dblCounterPartyHazardRateDrift = 0.002;
+		double dblCounterPartyHazardRateVolatility = 0.30;
+		double dblCounterPartyHazardRateInitial = 0.030;
+		double dblCounterPartyRecoveryRateDrift = 0.002;
+		double dblCounterPartyRecoveryRateVolatility = 0.02;
+		double dblCounterPartyRecoveryRateInitial = 0.30;
+		double dblBankFundingSpreadDrift = 0.00002;
+		double dblBankFundingSpreadVolatility = 0.002;
+
+		double[][] aadblCorrelation = new double[][] {
+			{1.00, 0.03,  0.07,  0.04,  0.05,  0.08,  0.00},  // PORTFOLIO
+			{0.03, 1.00,  0.26,  0.33,  0.21,  0.35,  0.13},  // CSA
+			{0.07, 0.26,  1.00,  0.45, -0.17,  0.07,  0.77},  // BANK HAZARD
+			{0.04, 0.33,  0.45,  1.00, -0.22, -0.54,  0.58},  // COUNTER PARTY HAZARD
+			{0.05, 0.21, -0.17, -0.22,  1.00,  0.47, -0.23},  // BANK RECOVERY
+			{0.08, 0.35,  0.07, -0.54,  0.47,  1.00,  0.01},  // COUNTER PARTY RECOVERY
+			{0.00, 0.13,  0.77,  0.58, -0.23,  0.01,  1.00}   // BANK FUNDING SPREAD
+		};
+
+		JulianDate dtSpot = DateUtil.Today();
+
+		double dblTimeWidth = dblTime / iNumStep;
+		JulianDate[] adtVertex = new JulianDate[iNumStep + 1];
+		CounterPartyGroupPath[] aCPGP = new CounterPartyGroupPath[iNumPath];
+		double[][] aadblPortfolio1Value = new double[iNumPath][iNumStep + 1];
+		double[][] aadblPortfolio2Value = new double[iNumPath][iNumStep + 1];
+		double[][] aadblCollateralBalance = new double[iNumPath][iNumStep + 1];
+		double dblBankFundingSpreadInitial = dblBankHazardRateInitial / (1. - dblBankRecoveryRateInitial);
+
+		DiffusionEvolver deATMSwapRateOffset = new DiffusionEvolver (
+			DiffusionEvaluatorLinear.Standard (
+				dblATMSwapRateOffsetDrift,
+				dblATMSwapRateOffsetVolatility
+			)
+		);
+
+		DiffusionEvolver deCSA = new DiffusionEvolver (
+			DiffusionEvaluatorLogarithmic.Standard (
+				dblCSADrift,
+				dblCSAVolatility
+			)
+		);
+
+		DiffusionEvolver deBankHazardRate = new DiffusionEvolver (
+			DiffusionEvaluatorLogarithmic.Standard (
+				dblBankHazardRateDrift,
+				dblBankHazardRateVolatility
+			)
+		);
+
+		DiffusionEvolver deBankRecoveryRate = new DiffusionEvolver (
+			DiffusionEvaluatorLogarithmic.Standard (
+				dblBankRecoveryRateDrift,
+				dblBankRecoveryRateVolatility
+			)
+		);
+
+		DiffusionEvolver deCounterPartyHazardRate = new DiffusionEvolver (
+			DiffusionEvaluatorLogarithmic.Standard (
+				dblCounterPartyHazardRateDrift,
+				dblCounterPartyHazardRateVolatility
+			)
+		);
+
+		DiffusionEvolver deCounterPartyRecoveryRate = new DiffusionEvolver (
+			DiffusionEvaluatorLogarithmic.Standard (
+				dblCounterPartyRecoveryRateDrift,
+				dblCounterPartyRecoveryRateVolatility
+			)
+		);
+
+		DiffusionEvolver deBankFundingSpread = new DiffusionEvolver (
+			DiffusionEvaluatorLinear.Standard (
+				dblBankFundingSpreadDrift,
+				dblBankFundingSpreadVolatility
+			)
+		);
+
+		for (int i = 0; i < iNumPath; ++i) {
+			double[][] aadblNumeraire = Matrix.Transpose (
+				Path (
+					aadblCorrelation,
+					iNumVertex
+				)
+			);
+
+			aadblPortfolio1Value[i] = SwapPortfolioValueRealization (
+				deATMSwapRateOffset,
+				dblATMSwapRateOffsetStart1,
+				aadblNumeraire[0],
+				iNumVertex,
+				dblTime,
+				dblTimeWidth,
+				dblTimeMaturity1,
+				dblSwapNotional1
+			);
+
+			aadblPortfolio2Value[i] = SwapPortfolioValueRealization (
+				deATMSwapRateOffset,
+				dblATMSwapRateOffsetStart2,
+				aadblNumeraire[0],
+				iNumVertex,
+				dblTime,
+				dblTimeWidth,
+				dblTimeMaturity2,
+				dblSwapNotional2
+			);
+
+			double[] adblCSA = NumeraireValueRealization (
+				deCSA,
+				dblCSAInitial,
+				dblTime,
+				dblTimeWidth,
+				aadblNumeraire[1],
+				iNumStep
+			);
+
+			double[] adblBankHazardRate = NumeraireValueRealization (
+				deBankHazardRate,
+				dblBankHazardRateInitial,
+				dblTime,
+				dblTimeWidth,
+				aadblNumeraire[2],
+				iNumStep
+			);
+
+			double[] adblCounterPartyHazardRate = NumeraireValueRealization (
+				deCounterPartyHazardRate,
+				dblCounterPartyHazardRateInitial,
+				dblTime,
+				dblTimeWidth,
+				aadblNumeraire[3],
+				iNumStep
+			);
+
+			double[] adblBankRecoveryRate = NumeraireValueRealization (
+				deBankRecoveryRate,
+				dblBankRecoveryRateInitial,
+				dblTime,
+				dblTimeWidth,
+				aadblNumeraire[4],
+				iNumStep
+			);
+
+			double[] adblCounterPartyRecoveryRate = NumeraireValueRealization (
+				deCounterPartyRecoveryRate,
+				dblCounterPartyRecoveryRateInitial,
+				dblTime,
+				dblTimeWidth,
+				aadblNumeraire[5],
+				iNumStep
+			);
+
+			double[] adblBankFundingSpread = NumeraireValueRealization (
+				deBankFundingSpread,
+				dblBankFundingSpreadInitial,
+				dblTime,
+				dblTimeWidth,
+				aadblNumeraire[6],
+				iNumStep
+			);
+
+			NumeraireVertex[] aNV = new NumeraireVertex [iNumStep + 1];
+			CollateralGroupVertex[] aCGV1 = new CollateralGroupVertex[iNumStep + 1];
+			CollateralGroupVertex[] aCGV2 = new CollateralGroupVertex[iNumStep + 1];
+
+			for (int j = 0; j <= iNumStep; ++j) {
+				aNV[j] = NumeraireVertex.Standard (
+					adtVertex[j] = dtSpot.addMonths (6 * j + 6),
+					adblCSA[j],
+					Math.exp (-0.5 * adblBankHazardRate[j] * (j + 1)),
+					adblBankRecoveryRate[j],
+					adblBankFundingSpread[j],
+					Math.exp (-0.5 * adblCounterPartyHazardRate[j] * (j + 1)),
+					adblCounterPartyRecoveryRate[j]
+				);
+
+				aadblCollateralBalance[i][j] = 0.;
+
+				aCGV1[j] = new CollateralGroupVertex (
+					adtVertex[j],
+					aadblPortfolio1Value[i][j],
+					0.,
+					0.
+				);
+
+				aCGV2[j] = new CollateralGroupVertex (
+					adtVertex[j],
+					aadblPortfolio2Value[i][j],
+					0.,
+					0.
+				);
+			}
+
+			NumerairePath np = new NumerairePath (aNV);
+
+			CollateralGroupPath[] aCGP = new CollateralGroupPath[] {
+				new CollateralGroupPath (aCGV1),
+				new CollateralGroupPath (aCGV2)
+			};
+
+			aCPGP[i] = new CounterPartyGroupPath (
+				new NettingGroupPath[] {
+					new NettingGroupPath (
+						aCGP,
+						np
+					)
+				},
+				new FundingGroupPath[] {
+					new FundingGroupPath (
+						aCGP,
+						np
+					)
+				}
+			);
+		}
+
+		return new CounterPartyGroupAggregator (aCPGP);
+	}
+
+	private static final void CPGDDump (
+		final String strHeader,
+		final CounterPartyGroupDigest cpgd)
+		throws Exception
+	{
+		System.out.println();
+
+		UnivariateDiscreteThin udtUCVA = cpgd.ucva();
+
+		UnivariateDiscreteThin udtFTDCVA = cpgd.ftdcva();
+
+		UnivariateDiscreteThin udtCVACL = cpgd.cvacl();
+
+		UnivariateDiscreteThin udtCVA = cpgd.cva();
+
+		UnivariateDiscreteThin udtFVA = cpgd.fva();
+
+		UnivariateDiscreteThin udtFDA = cpgd.fda();
+
+		UnivariateDiscreteThin udtFCA = cpgd.fca();
+
+		UnivariateDiscreteThin udtFBA = cpgd.fba();
+
+		UnivariateDiscreteThin udtSFVA = cpgd.sfva();
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+
+		System.out.println (strHeader);
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+
+		System.out.println (
+			"\t||  OODLE  =>  UCVA   | FTDCVA  |  CVACL  |   CVA   |   DVA   |   FDA   |   FCA   |   FBA   |   SFVA  ||"
+		);
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+
+		System.out.println (
+			"\t|| Average => " +
+			FormatUtil.FormatDouble (udtUCVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.average(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.average(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t|| Minimum => " +
+			FormatUtil.FormatDouble (udtUCVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.minimum(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.minimum(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t|| Maximum => " +
+			FormatUtil.FormatDouble (udtUCVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.maximum(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.maximum(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t||  Error  => " +
+			FormatUtil.FormatDouble (udtUCVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.error(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.error(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+	}
+
+	private static final void CPGDDiffDump (
+		final String strHeader,
+		final CounterPartyGroupDigest cpgdBase,
+		final CounterPartyGroupDigest cpgdExpanded)
+		throws Exception
+	{
+		System.out.println();
+
+		UnivariateDiscreteThin udtUCVA = cpgdBase.ucva();
+
+		UnivariateDiscreteThin udtFTDCVA = cpgdBase.ftdcva();
+
+		UnivariateDiscreteThin udtCVACL = cpgdBase.cvacl();
+
+		UnivariateDiscreteThin udtCVA = cpgdBase.cva();
+
+		UnivariateDiscreteThin udtFVA = cpgdBase.fva();
+
+		UnivariateDiscreteThin udtFDA = cpgdBase.fda();
+
+		UnivariateDiscreteThin udtFCA = cpgdBase.fca();
+
+		UnivariateDiscreteThin udtFBA = cpgdBase.fba();
+
+		UnivariateDiscreteThin udtSFVA = cpgdBase.sfva();
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+
+		System.out.println (strHeader);
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+
+		System.out.println (
+			"\t||  OODLE  =>  UCVA   | FTDCVA  |  CVACL  |   CVA   |   DVA   |   FDA   |   FCA   |   FBA   |   SFVA  ||"
+		);
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+
+		System.out.println (
+			"\t|| Average => " +
+			FormatUtil.FormatDouble (udtUCVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.average(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.average(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.average(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t|| Minimum => " +
+			FormatUtil.FormatDouble (udtUCVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.minimum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.minimum(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.minimum(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t|| Maximum => " +
+			FormatUtil.FormatDouble (udtUCVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.maximum(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.maximum(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.maximum(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t||  Error  => " +
+			FormatUtil.FormatDouble (udtUCVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFTDCVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVACL.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtCVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFVA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFDA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFCA.error(), 2, 2, 1.) + "  | " +
+			FormatUtil.FormatDouble (udtFBA.error(), 2, 2, 1.) + "  | " + 
+			FormatUtil.FormatDouble (udtSFVA.error(), 2, 2, 1.) + "  ||"
+		);
+
+		System.out.println (
+			"\t||----------------------------------------------------------------------------------------------------||"
+		);
+	}
+
+	public static final void main (
+		final String[] astrArgs)
+		throws Exception
+	{
+		EnvManager.InitEnv ("");
+
+		CounterPartyGroupAggregator cpgaGround = Mix (
+			5.,
+			0.,
+			100.,
+			5.,
+			0.,
+			0.
+		);
+
+		CPGDDump (
+			"\t||                                   GROUND BOOK VALUATION METRICS                                    ||",
+			cpgaGround.digest()
+		);
+
+		CounterPartyGroupAggregator cpgaExtended = Mix (
+			5.,
+			0.,
+			100.,
+			5.,
+			0.,
+			1.
+		);
+
+		CPGDDump (
+			"\t||                                  EXTENDED BOOK VALUATION METRICS                                   ||",
+			cpgaExtended.digest()
+		);
+
+		/* OTCAccountingScheme oasFCAFBA = new OTCAccountingSchemeFCAFBA (cpgaBase);
+
+		OTCAccountingScheme oasFVAFDA = new OTCAccountingSchemeFVAFDA (cpgaBase);
+
+		System.out.println (oasFVAFDA.feePolicy (cpgaIncrement));
+
+		System.out.println (oasFVAFDA.feePolicy (cpgaIncrement));
+
+		System.out.println ("\t|| " +
+			FormatUtil.FormatDouble (oasFCAFBA.contraAssetAdjustment(), 1, 4, 1.) + " | " +
+			FormatUtil.FormatDouble (oasFCAFBA.contraLiabilityAdjustment(), 1, 4, 1.) + " || " +
+			FormatUtil.FormatDouble (oasFVAFDA.contraAssetAdjustment(), 1, 4, 1.) + " | " +
+			FormatUtil.FormatDouble (oasFVAFDA.contraLiabilityAdjustment(), 1, 4, 1.) + " ||"
+		); */
 	}
 }
